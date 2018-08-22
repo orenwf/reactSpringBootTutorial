d56e4d8ae436edb053166bc2d1851f87617fb1a2
Oren <ofriedman@acm.org>
2018-08-22 11:05:08 -0400

    initial

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data @Entity public 
+class Employee {
+	
+	@Id @GeneratedValue private
+	Long id;
+	private
+	String firstName;
+	private
+	String lastName;
+	private
+	String description;
+	
+	private
+	Employee() {}
+	
+	public
+	Employee(String firstName, String lastName, String description) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.description = description;
+	}
+	
+}
